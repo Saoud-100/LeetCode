49b4e4a0fda54a86a5f7168dae79ee1b00dfcb97
Saoud <Mohd.Saoud096@gmail.com>
2026-09-19 21:04:19 +0530

    Time: 0 ms (100%), Space: 8.6 MB (31.5%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int openCount = 0, closeCount = 0;
+        stack<char> st;
+
+        for(auto ch : s){
+            if(ch == '('){
+                st.push(ch);
+                openCount++;
+            }
+            else{
+                if(!st.empty()){
+                    st.pop();
+                    openCount--;
+                }
+                else{
+                    closeCount++;
+                }
+            }
+        }
+        return openCount + closeCount;
+    }
+};
